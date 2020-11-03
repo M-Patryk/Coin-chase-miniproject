fd0e9098cb345d6b89dc1c12865ad051a164949c
PatrykM <patrykxmajchrzak@gmail.com>
2020-11-03 15:51:46 +0100

    Cleaned the code a little bit by adding functions

--- app.js
+++ app.js
@@ -10,38 +10,42 @@ function isTouching(a, b) {
 	);
 }
 
-const character = document.querySelector('#player')
-const coin = document.querySelector('#coin')
+const character = document.querySelector('#player');
+const coin = document.querySelector('#coin');
 
-window.addEventListener('keyup', function(e){
-	if(e.key === 'ArrowDown'){
-		const currentTop = extractPosition(character.style.top)
-		character.style.top = `${currentTop + 50}px`
-	} else if (e.key === 'ArrowUp'){
-		const currentTop = extractPosition(character.style.top)
-		character.style.top = `${currentTop - 50}px`
-	} else if (e.key === 'ArrowLeft'){
-		const currentLeft = extractPosition(character.style.left)
-		character.style.transform = 'scale(-1,1)'
-		character.style.left = `${currentLeft - 50}px`
-	} else if (e.key === 'ArrowRight'){
-		const currentLeft = extractPosition(character.style.left)
-		character.style.transform = 'scale(1,1)'
-		character.style.left = `${currentLeft + 50}px`
+window.addEventListener('keyup', function(e) {
+	if (e.key === 'ArrowDown') {
+		moveVertical(character, 50);
+	} else if (e.key === 'ArrowUp') {
+		moveVertical(character, -50);
+	} else if (e.key === 'ArrowLeft') {
+		character.style.transform = 'scale(-1,1)';
+		moveHorizontal(character, -50);
+	} else if (e.key === 'ArrowRight') {
+		character.style.transform = 'scale(1,1)';
+		moveHorizontal(character, 50);
 	}
-	if(isTouching(character, coin)) moveCoin()
-})
+	if (isTouching(character, coin)) moveCoin();
+});
+
+const moveVertical = (element, amount) => {
+	const currentTop = extractPosition(element.style.top);
+	element.style.top = `${currentTop + amount}px`;
+};
+
+const moveHorizontal = (element, amount) => {
+	const currentLeft = extractPosition(element.style.left);
+	element.style.left = `${currentLeft + amount}px `;
+};
 
 const extractPosition = (pos) => {
-	if(!pos) return 100
-	return parseInt(pos.slice(0,-2))
-}
+	if (!pos) return 100;
+	return parseInt(pos.slice(0, -2));
+};
 
 const moveCoin = () => {
-	const coinLeftPos = Math.floor(Math.random() * window.innerWidth)
-	const coinTopPos = Math.floor(Math.random() * window.innerHeight)
-	return coin.style.top = `${coinTopPos}px`,
-	coin.style.left = `${coinLeftPos}px`
-	
-}
-moveCoin()
+	const coinLeftPos = Math.floor(Math.random() * window.innerWidth);
+	const coinTopPos = Math.floor(Math.random() * window.innerHeight);
+	return (coin.style.top = `${coinTopPos}px`), (coin.style.left = `${coinLeftPos}px`);
+};
+moveCoin();
